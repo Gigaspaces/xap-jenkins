546629f2fa5c8c44c1001edc05d3b3327bc80007
Barak Bar Orion <barak.bar@gmail.com>
2016-05-23 10:04:38 +0300

    Add docker files

---
+++ jenkins-docker/connect.sh
@@ -0,0 +1,1 @@
+docker exec -it build-jenkins /bin/bash
---
+++ jenkins-docker/run-without-wd.sh
@@ -0,0 +1,1 @@
+docker run -e JAVA_OPTS=-Duser.timezone=Asia/Jerusalem -d -p 8080:8080 --volume `pwd`/../jenkins_backup/jenkins_home:/var/jenkins_home    --volume /home/xap/.ssh:/var/jenkins_home/.ssh --name build-jenkins xap/jenkins
