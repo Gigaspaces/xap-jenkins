00d26d9ac456820b833276ce3da7531fef6b6d79
Barak Bar Orion <barak.bar@gmail.com>
2016-05-23 10:49:21 +0300

    Add job

--- jenkins-docker/run.sh
+++ jenkins-docker/run.sh
@@ -1,2 +1,2 @@
-docker run -e JAVA_OPTS=-Duser.timezone=Asia/Jerusalem -d -p 8080:8080 --volume `pwd`/../jenkins_home:/var/jenkins_home --volume `pwd`/../workspaces:/var/workspaces   --volume /home/xap/.ssh:/home/jenkins/.ssh  --volume `pwd`/../m2:/home/jenkins/.m2  --name build-jenkins xap/jenkins
+docker run -e JAVA_OPTS=-Duser.timezone=Asia/Jerusalem -d -p 8080:8080 --volume `pwd`/../jenkins_home:/var/jenkins_home --volume `pwd`/../workspaces:/var/workspaces   --volume /home/${USER}/.ssh:/home/jenkins/.ssh  --volume `pwd`/../m2:/home/jenkins/.m2  --name build-jenkins xap/jenkins
 
