921b6c1b56addf9df70c96cef0a93762d27a17c0
Barak Bar Orion <barak.bar@gmail.com>
2016-06-21 13:29:51 +0300

    Add xap project that can be use to build both open and close

--- jenkins-docker/jenkins.sh
+++ jenkins-docker/jenkins.sh
@@ -30,7 +30,7 @@ find /usr/share/jenkins/ref/ -type f -exec bash -c "copy_reference_file '{}'" \;
 
 # if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
-  eval "exec java $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war $JENKINS_OPTS \"\$@\""
+  eval "exec java $JAVA_OPTS -Dorg.jenkinsci.plugins.gitclient.Git.timeOut=60 -jar /usr/share/jenkins/jenkins.war $JENKINS_OPTS \"\$@\""
 fi
 
 # As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
