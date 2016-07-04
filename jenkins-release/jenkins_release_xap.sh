a498350e63f53d65282e1f7950feb84358f6b019
kobi <kobikis@gmail.com>
2016-07-04 08:38:47 +0300

    fixed small issues

--- jenkins-release/jenkins_release_xap.sh
+++ jenkins-release/jenkins_release_xap.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+set -e
+
+. setenv.sh
+
 function release_xap {
     local mode=$1
 
@@ -24,43 +28,47 @@ function release_xap {
     announce_step "disabling xap-release and xap-continuous jenkins jobs"
     get_jenkins_job_config "xap-release" "xap_release_config.xml"
     get_jenkins_job_config "xap-continuous" "xap_continuous_config.xml"
-    stop_jenkins_triggers "xap_release_config.xml"
-    stop_jenkins_triggers "xap_continuous_config.xml"
+    stop_jenkins_all_trigger "xap_release_config.xml"
+    stop_jenkins_all_trigger "xap_continuous_config.xml"
     post_jenkins_job_config "xap-release" "xap_release_config.xml"
     post_jenkins_job_config "xap-continuous" "xap_continuous_config.xml"
+
+
 
     #jenkins xap-release job
     announce_step "updating xap-release mode to $mode and BRANCH_NAME to $BRANCH_NAME and trigger to 0 16 * * *"
     get_jenkins_job_config "xap-release" "xap_release_config.xml"
     change_mode "$mode" "xap_release_config.xml"
-    change_branch "$BRANCH_NAME" "xap_release_config.xml"
+    change_branch "\$XAP_VERSION-\$MILESTONE" "xap_release_config.xml"
     if [ "$MILESTONE" -eq "ga" ]
     then
-        change_tag "$VERSION-$MILESTONE-RELEASE" "xap_release_config.xml"
+        change_tag "\$XAP_VERSION-\$MILESTONE-RELEASE" "xap_release_config.xml"
     else
-        change_tag "$VERSION-$MILESTONE-MILESTONE" "xap_release_config.xml"
+        change_tag "\$XAP_VERSION-\$MILESTONE-MILESTONE" "xap_release_config.xml"
     fi
-    start_jenkins_triggers "xap_release_config.xml" "0 16 * * *"
+    start_jenkins_timer_trigger "xap_release_config.xml" "0 16 * * *"
     post_jenkins_job_config "xap-release" "xap_release_config.xml"
 
     #jenkins xap-continuous job - xap-continuous is disabled do also xap-continuous-master is disabled
     announce_step "coping xap-continuous to xap-continuous-master"
+    get_jenkins_job_config "xap-continuous" "xap_continuous_config.xml"
     copy_jenkins_job "xap-continuous" "xap-continuous-master"
 
     announce_step "incrementing XAP_BUILD_NUMBER at xap-continuous-master"
     get_jenkins_job_config "xap-continuous-master" "xap_continuous_master_config.xml"
-    increment_build_number "xap_continuous_master_config.xml"
+    increment_build_number "xap_continuous_master_config.xml" "10"
     increment_milestone_number "xap_continuous_master_config.xml"
-    post_jenkins_job_config "xxap-continuous-master" "xap_continuous_master_config.xml"
+    post_jenkins_job_config "xap-continuous-master" "xap_continuous_master_config.xml"
 
     announce_step "changing BRANCH_NAME to $BRANCH_NAME at xap-continuous job and trigger to * * * * *"
     get_jenkins_job_config "xap-continuous" "xap_continuous_config.xml"
-    change_branch "$BRANCH_NAME" "xap_continuous_config.xml"
-    start_jenkins_triggers "xap_continuous_config.xml" "* * * * *"
+    change_branch "\$XAP_VERSION-\$MILESTONE" "xap_continuous_config.xml"
+    start_jenkins_timer_trigger "xap_continuous_config.xml" "0 6 * * *"
+    start_jenkins_scm_trigger "xap_continuous_config.xml" "* * * * *"
     post_jenkins_job_config "xap-continuous" "xap_continuous_config.xml"
 
-    announce_step "add branch $BRANCH_NAME to newman reporter cron"
-    append_branch_to_newman_cron "$BRANCH_NAME"
+    #announce_step "add branch $BRANCH_NAME to newman reporter cron"
+    #append_branch_to_newman_cron "$BRANCH_NAME"
 }
 
 function back_to_nightly_release_xap {
@@ -70,25 +78,27 @@ function back_to_nightly_release_xap {
     get_jenkins_job_config "xap-release" "xap_release_config.xml"
     get_jenkins_job_config "xap-continuous" "xap_continuous_config.xml"
     get_jenkins_job_config "xap-continuous-master" "xap_continuous_master_config.xml"
-    stop_jenkins_triggers "xap_release_config.xml"
-    stop_jenkins_triggers "xap_continuous_config.xml"
-    stop_jenkins_triggers "xap_continuous_master_config.xml"
+    stop_jenkins_all_trigger "xap_release_config.xml"
+    stop_jenkins_all_trigger "xap_continuous_config.xml"
+    stop_jenkins_all_trigger "xap_continuous_master_config.xml"
     post_jenkins_job_config "xap-release" "xap_release_config.xml"
     post_jenkins_job_config "xap-continuous" "xap_continuous_config.xml"
     post_jenkins_job_config "xap-continuous-master" "xap_continuous_master_config.xml"
 
     announce_step "updating xap-release mode to NIGHTLY and BRANCH_NAME to master and trigger to 0 16 * * *"
     get_jenkins_job_config "xap-release" "xap_release_config.xml"
-    change_mode "NIGHTLY"
-    change_branch "master"
-    start_jenkins_triggers "xap_release_config.xml" "0 16 * * *"
-    increment_build_number "xap_continuous_master_config.xml"
+    change_mode "NIGHTLY" "xap_release_config.xml"
+    change_branch "master" "xap_release_config.xml"
+    start_jenkins_scm_trigger "xap_release_config.xml" "0 16 * * *"
+    increment_milestone_number "xap_release_config.xml"
+    increment_build_number "xap_release_config.xml" "1"
     post_jenkins_job_config "xap-release" "xap_release_config.xml"
 
     announce_step "delete xap-continuous job"
     delete_jenkins_job "xap-continuous"
 
     announce_step "coping xap-continuous-master to xap-continuous"
+    get_jenkins_job_config "xap-continuous-master" "xap_continuous_master_config.xml"
     copy_jenkins_job "xap-continuous-master" "xap-continuous"
 
     announce_step "deleting xap-continuous-master"
@@ -96,29 +106,35 @@ function back_to_nightly_release_xap {
 
     announce_step "updating xap-continuous trigger to * * * * *"
     get_jenkins_job_config "xap-continuous" "xap_continuous_config.xml"
-    start_jenkins_triggers "xap_continuous_config.xml" "* * * * *"
+    start_jenkins_timer_trigger "xap_continuous_config.xml" "0 6 * * *"
+    start_jenkins_scm_trigger "xap_continuous_config.xml" "* * * * *"
     post_jenkins_job_config "xap-continuous" "xap_continuous_config.xml"
 
-    announce_step "return branch master to newman reporter cron"
-    append_branch_to_newman_cron "master"
+    #announce_step "return branch master to newman reporter cron"
+    #append_branch_to_newman_cron "master"
 }
 
 function copy_jenkins_job {
-    local job_name="$1"
-    local new_job_name="$2"
-    cmd="curl -X POST http://"$JENKINS_HOST":"$JENKINS_PORT"/createItem?name="$new_job_name"&mode=copy&from="$job_name""
-    eval $cmd
+    local from_job_name="$1"
+    local to_job_name="$2"
+
+    curl -s --data ' ' "http://"$JENKINS_USER":"$JENKINS_PASSWORD"@"$JENKINS_HOST":"$JENKINS_PORT"/createItem?name="$to_job_name"&mode=copy&from="$from_job_name
+    curl -s --data disable "http://"$JENKINS_USER":"$JENKINS_PASSWORD"@"$JENKINS_HOST":"$JENKINS_PORT"/job/$to_job_name/disable"
+	curl -s --data enable "http://"$JENKINS_USER":"$JENKINS_PASSWORD"@"$JENKINS_HOST":"$JENKINS_PORT"/job/$to_job_name/enable"
+
+    local r="$?"
     if [ "$r" -ne 0 ]
     then
-        echo "[ERROR] Failed to copy $job_name to $new_job_name, cmd: $cmd, exit code is: $r"
+        echo "[ERROR] Failed to copy $job_name to $new_job_name, exit code is: $r"
         exit "$r"
     fi
 }
 
 function delete_jenkins_job {
     local job_name="$1"
-    cmd="curl -X POST http://"$JENKINS_HOST":"$JENKINS_PORT"/job/"$job_name"/deDelete"
-    eval $cmd
+    cmd="curl -X POST http://"$JENKINS_USER":"$JENKINS_PASSWORD"@"$JENKINS_HOST":"$JENKINS_PORT"/job/"$job_name"/doDelete"
+    eval "$cmd"
+    local r="$?"
     if [ "$r" -ne 0 ]
     then
         echo "[ERROR] Failed to delete jenkins job $job_name, cmd: $cmd, exit code is: $r"
@@ -126,31 +142,48 @@ function delete_jenkins_job {
     fi
 }
 
-function stop_jenkins_triggers{
+function stop_jenkins_all_trigger {
     local jenkins_config_file="$1"
     sed -i '/<hudson.triggers.TimerTrigger>/{N; s/<spec>.*<\/spec>/<spec><\/spec>/}' "$jenkins_config_file"
     sed -i '/<hudson.triggers.SCMTrigger>/{N; s/<spec>.*<\/spec>/<spec><\/spec>/}' "$jenkins_config_file"
 }
 
-function start_jenkins_triggers{
+function stop_jenkins_timer_trigger {
+    local jenkins_config_file="$1"
+    sed -i '/<hudson.triggers.TimerTrigger>/{N; s/<spec>.*<\/spec>/<spec><\/spec>/}' "$jenkins_config_file"
+}
+
+function stop_jenkins_scm_trigger {
+    local jenkins_config_file="$1"
+    sed -i '/<hudson.triggers.SCMTrigger>/{N; s/<spec>.*<\/spec>/<spec><\/spec>/}' "$jenkins_config_file"
+}
+
+function start_jenkins_timer_trigger {
     local jenkins_config_file="$1"
     local cron_pattern="$2"
 
-    sed -i '/<hudson.triggers.TimerTrigger>/{N; s/<spec>.*<\/spec>/<spec>'$cron_pattern'<\/spec>/}' "$jenkins_config_file"
-    sed -i '/<hudson.triggers.SCMTrigger>/{N; s/<spec>.*<\/spec>/<spec>'$cron_pattern'<\/spec>/}' "$jenkins_config_file"
+    sed -i '/<hudson.triggers.TimerTrigger>/{N; s/<spec>.*<\/spec>/<spec>'"$cron_pattern"'<\/spec>/g;s/<spec\/>/<spec>'"$cron_pattern"'<\/spec>/g}' "$jenkins_config_file"
 }
 
-function append_branch_to_newman_cron{
+function start_jenkins_scm_trigger {
+    local jenkins_config_file="$1"
+    local cron_pattern="$2"
+
+    sed -i '/<hudson.triggers.SCMTrigger>/{N; s/<spec>.*<\/spec>/<spec>'"$cron_pattern"'<\/spec>/g;s/<spec\/>/<spec>'"$cron_pattern"'<\/spec>/g}' "$jenkins_config_file"
+}
+
+function append_branch_to_newman_cron {
     local branch="$1"
     sshpass -e ssh "$XAP_NEWMAN_USER"@"$XAP_NEWMAN_HOST" -C "sed -ri 's/crons.suitediff.branch = .*/crons.suitediff.branch = '$branch'/'g /home/xap/newman-analytics/resources/crons/suitediff/suitediff.properties"
 }
 
-function get_jenkins_job_config{
+function get_jenkins_job_config {
     local jenkins_job="$1"
     local jenkins_config_file="$2"
     #Get the current configuration and save it locally
     cmd="curl -X GET http://"$JENKINS_USER":"$JENKINS_PASSWORD"@"$JENKINS_HOST":"$JENKINS_PORT"/job/"$jenkins_job"/config.xml -o "$jenkins_config_file""
-    eval $cmd
+    eval "$cmd"
+    local r="$?"
     if [ "$r" -ne 0 ]
     then
         echo "[ERROR] Failed to get jenkins config $jenkins_job, cmd: $cmd, exit code is: $r"
@@ -158,12 +191,13 @@ function get_jenkins_job_config{
     fi
 }
 
-function post_jenkins_job_config{
+function post_jenkins_job_config {
     local jenkins_job="$1"
     local jenkins_config_file="$2"
     #Update the configuration via posting a local configuration file
     cmd="curl -X POST http://"$JENKINS_USER":"$JENKINS_PASSWORD"@"$JENKINS_HOST":"$JENKINS_PORT"/job/"$jenkins_job"/config.xml --data-binary "@"$jenkins_config_file"""
-    eval $cmd
+    eval "$cmd"
+    local r="$?"
     if [ "$r" -ne 0 ]
     then
         echo "[ERROR] Failed to get jenkins config $jenkins_job, cmd: $cmd, exit code is: $r"
@@ -171,14 +205,16 @@ function post_jenkins_job_config{
     fi
 }
 
-function increment_build_number{
+function increment_build_number {
     local jenkins_config_file="$1"
+    local add="$2"
+
     local version_number=$(sed -n '/<name>XAP_BUILD_NUMBER<\/name>/{N; /.*/{N; /<defaultValue>.*<\/defaultValue>/p}}' "$jenkins_config_file" | grep defaultValue | sed -n '/defaultValue/{s/.*<defaultValue>\(.*\)<\/defaultValue>.*/\1/;p}')
-    (($version_number++))
+    let "version_number = version_number + $add"
     sed -i '/<name>XAP_BUILD_NUMBER<\/name>/{N; /.*/{N; s/<defaultValue>.*<\/defaultValue>/<defaultValue>'$version_number'<\/defaultValue>/}}' "$jenkins_config_file"
 }
 
-function increment_milestone_number{
+function increment_milestone_number {
     local jenkins_config_file="$1"
     local milestone=$(sed -n '/<name>MILESTONE<\/name>/{N; /.*/{N; /<defaultValue>.*<\/defaultValue>/p}}' "$jenkins_config_file" | grep defaultValue | sed -n '/defaultValue/{s/.*<defaultValue>\(.*\)<\/defaultValue>.*/\1/;p}')
     milestone_number=${milestone:1:${#milestone}}
@@ -186,19 +222,19 @@ function increment_milestone_number{
     sed -i '/<name>MILESTONE<\/name>/{N; /.*/{N; s/<defaultValue>.*<\/defaultValue>/<defaultValue>m'$milestone_number'<\/defaultValue>/}}' "$jenkins_config_file"
 }
 
-function change_mode{
+function change_mode {
     local mode="$1"
     local jenkins_config_file="$2"
     sed -i '/<a class="string-array">/{N; s/<string>.*<\/string>/<string>'"$mode"'<\/string>/}' "$jenkins_config_file"
 }
 
-function change_branch{
+function change_branch {
     local branch="$1"
     local jenkins_config_file="$2"
     sed -i '/<name>BRANCH_NAME<\/name>/{N; /.*/{N; s/<defaultValue>.*<\/defaultValue>/<defaultValue>'"$branch"'<\/defaultValue>/}}' "$jenkins_config_file"
 }
 
-function change_tag{
+function change_tag {
     local tag="$1"
     local jenkins_config_file="$2"
     sed -i '/<name>TAG_NAME<\/name>/{N; /.*/{N; s/<defaultValue>.*<\/defaultValue>/<defaultValue>'"$tag"'<\/defaultValue>/}}' "$jenkins_config_file"
@@ -213,9 +249,9 @@ function create_release_branch {
 
 	if [ -z "$commit_sh" ]
 	then
-	    git checkout -b  "$BRANCH_NAME" "$commit_sh"
+	    git checkout -b "$BRANCH_NAME" master
 	else
-	    git branch "$BRANCH_NAME" master
+	    git checkout -b "$BRANCH_NAME" "$commit_sh"
 	fi
 	git push --set-upstream origin "$BRANCH_NAME"
     )
@@ -279,4 +315,6 @@ function announce_step {
     let start_time=$(date +'%s')
 }
 
-release_xap "MILESTONE"
+#release_xap "MILESTONE"
+
+back_to_nightly_release_xap
