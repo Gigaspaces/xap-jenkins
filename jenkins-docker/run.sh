859ddc36238e17b0424543b565214e3c76600c2c
yohanakh <yohana@yohanakhoury.com>
2019-04-08 12:16:52 +0300

    install deps script
    usermod
    
    install deps
    
    add m2s directory for all m2s
    
    Use dev java files
    
    Install wget
    
    Removed SBT
    
    Added JAVA_HOME to PATH
    
    Allow running docker interactivly
    
    Fixed if
    
    Create required folders if they don't exist
    
    added --rm
    
    mount /etc/hosts
    
    use add-host for newman-server hostname
    
    fixed typo
    
    install jdk 8_191
    
    changed path of ssh keys
    
    added ssh to git ignore

--- jenkins-docker/run.sh
+++ jenkins-docker/run.sh
@@ -1,2 +1,44 @@
 #!/usr/bin/env bash
-docker run -e JAVA_OPTS=-Duser.timezone=Asia/Jerusalem -d -p 8080:8080 -p 50000:50000 --volume `pwd`/../jenkins_home:/var/jenkins_home --volume `pwd`/../workspaces:/var/workspaces --volume /home/${USER}/.ssh:/home/jenkins/.ssh  --volume `pwd`/../m2:/home/jenkins/.m2 --volume `pwd`/../m2_release:/home/jenkins/.m2_release --volume `pwd`/../m2_release_xap:/home/jenkins/.m2_release_xap --volume `pwd`/../m2_ie_release:/home/jenkins/.m2_ie_release --volume `pwd`/../xap_scripts:/home/jenkins/xap_scripts  --volume `pwd`/../ie_scripts:/home/jenkins/ie_scripts --volume `pwd`/../spark:/home/jenkins/spark --volume `pwd`/../scala:/home/jenkins/scala -e SCALA_HOME=/home/jenkins/scala/scala-latest -e SPARK_DIST=/home/jenkins/spark/spark.tgz --name build-jenkins xap/jenkins
+MODE="-d"
+if [[ -n "$1" ]]; then
+	if [[ "$1" == "-iii" ]]; then
+		MODE=""
+	else
+		MODE="$1"
+	fi
+fi
+
+WORKSPACES=`pwd`/../workspaces
+if [[ ! -e "${WORKSPACES}" ]]; then
+    mkdir -p ${WORKSPACES}
+fi
+M2=`pwd`/../m2
+if [[ ! -e "${M2}" ]]; then
+    mkdir -p ${M2}
+fi
+
+M2S=`pwd`/../m2s
+if [[ ! -e "${M2S}" ]]; then
+    mkdir -p ${M2S}
+fi
+
+#NEWMAN_SERVER_IP is the IP of the host machine
+NEWMAN_SERVER_IP=$(hostname -I | awk '{ print $1 }')
+
+docker run -e JAVA_OPTS=-Duser.timezone=Asia/Jerusalem ${MODE} -p 8080:8080 -p 50000:50000 --rm \
+    --volume `pwd`/../jenkins_home:/var/jenkins_home \
+    --volume ${WORKSPACES}:/var/workspaces \
+    --volume ${M2S}:/var/m2s \
+    --volume `pwd`/../ssh:/home/jenkins/.ssh \
+    --volume ${M2}:/home/jenkins/.m2 \
+    --volume `pwd`/../m2_release:/home/jenkins/.m2_release \
+    --volume `pwd`/../m2_release_xap:/home/jenkins/.m2_release_xap \
+    --volume `pwd`/../m2_ie_release:/home/jenkins/.m2_ie_release \
+    --volume `pwd`/../xap_scripts:/home/jenkins/xap_scripts  \
+    --volume `pwd`/../ie_scripts:/home/jenkins/ie_scripts \
+    --volume `pwd`/../spark:/home/jenkins/spark \
+    --volume `pwd`/../scala:/home/jenkins/scala \
+    -e SCALA_HOME=/home/jenkins/scala/scala-latest \
+    -e SPARK_DIST=/home/jenkins/spark/spark.tgz \
+    --add-host "newman-server:${NEWMAN_SERVER_IP}" \
+    --name build-jenkins xap/jenkins
