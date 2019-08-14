0e86d4babd723f8015a76d6ee5b8092b9b09c585
yohanakh <yohana.khoury@gmail.com>
2019-08-14 16:47:32 +0300

    store ORIG_PATH and use it when changing elm versions

--- jenkins-docker/elm-env.sh
+++ jenkins-docker/elm-env.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
 
+if [[ -z "${ORIG_PATH}" ]]; then
+    export ORIG_PATH="${PATH}"
+fi
 
 function use_elm_version {
     local elmVersion=$1
     local globalDir="/home/jenkins/.npm-global_${elmVersion}"
     if [[ ! -e "${globalDir}" ]]; then echo "Elm version ${elmVersion} does not exist (${globalDir})";exit 1; fi
-    export PATH="${globalDir}/bin:${PATH}"
+    export PATH="${globalDir}/bin:${ORIG_PATH}"
 }
 export -f use_elm_version
 
