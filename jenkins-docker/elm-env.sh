44b99d8b1d44e9eae70121b7a6dd92ab36366381
alonshoham <alon.shoham@gmail.com>
2021-05-20 12:17:48 +0300

    downloading elm binaries from github instead of using npm install

--- jenkins-docker/elm-env.sh
+++ jenkins-docker/elm-env.sh
@@ -15,13 +15,20 @@ export -f use_elm_version
 
 function install_elm {
     local elmVersion=$1
-
     local globalDir="/home/jenkins/.npm-global_${elmVersion}"
     export PATH="${globalDir}/bin:${PATH}"
     if [[ -e "${globalDir}" ]]; then echo "Elm version ${elmVersion} already exist"; exit 1; fi
     mkdir "${globalDir}"
     npm config set prefix "${globalDir}"
-
+    if [[ "${elmVersion}" = "0.19.0" ]]
+    then
+        wget --no-verbose -O "${globalDir}"/elm.gz https://github.com/elm/compiler/releases/download/${elmVersion}/binary-for-linux-64-bit.gz
+    elif [[ "${elmVersion}" = "0.18.0" ]]
+    then
+        wget --no-verbose -O "${globalDir}"/elm.gz https://github.com/elm-lang/elm-platform/releases/download/0.18.0-exp/elm-platform-linux-64bit.tar.gz
+    fi
+    gunzip "${globalDir}"/elm.gz
+    chmod +x "${globalDir}"/elm
     use_elm_version ${elmVersion}
     #npm install -g elm@${elmVersion}
 }
