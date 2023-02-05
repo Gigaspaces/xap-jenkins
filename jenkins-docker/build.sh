96bda8a82879f69a372899fbfcb547bb2a70cbfd
Ariel Shatil <ariel.shatil@gigaspaces.com>
2023-02-06 00:05:57 +0200

    Remove imc-srv references

--- jenkins-docker/build.sh
+++ jenkins-docker/build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build --build-arg STORAGE_SERVER=${STORAGE_SERVER=imc-srv01} -t xap/jenkins .
+docker build --build-arg STORAGE_SERVER=${STORAGE_SERVER=gs-storage-server.s3.amazonaws.com} -t xap/jenkins .
